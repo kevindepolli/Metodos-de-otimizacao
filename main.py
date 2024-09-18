004e5cccdc6f88c694d058949437c44546d3fe74
Kevin Depolli <kevindepollidcf@gmail.com>
2024-09-18 17:53:49 -0300

    Ajustado alguns critéios referentes a avaliação

--- main.py
+++ main.py
@@ -4,34 +4,60 @@ from Contrutiva import Construtiva
 from Vizinho import Vizinho
 from simulated_annealing import simulated_annealing
 
+
 def calcular_fo(maquinas, pesos):
     maquinas_pesos = [sum(pesos[tarefa] for tarefa in maquina) for maquina in maquinas]
     return max(maquinas_pesos)
 
+
+def exibir_maquina(maquinas, pesos):
+    for i, maquina in enumerate(maquinas, start=1):
+        print(f"Máquina {i}: {list(maquina)}")
+    print("FO: ", calcular_fo(maquinas, pesos))
+
+
 def execucao(numero_maquinas):
-    inicio = time.time()
 
     # nome_arquivo_grafo = "grafos/grafo.txt"
     nome_arquivo_grafo = "grafos/HAHN.IN2"
     nome_arquivo_solucoes = "solucoes.txt"
+    tempo_parada = 9999999999
+    iteracoes_refinamento = 5
+
+    taxa_resfriamento = 0.998
+    equilibrio_termico = 20
+    temperatura_inicial = 1000
+    temperatura_congelamento = 0.0001
+
+    inicio_construtiva = time.time()
 
     construtiva = Construtiva(nome_arquivo_grafo, numero_maquinas)
+    fim_construtiva = time.time()
+
+    print("------------------------------------------")
+    print("Solução Inicial:")
     construtiva.maquina.exibir_resultado()
 
-    solucao = simulated_annealing(0.998, 20, 1000, 0.0001, construtiva.maquina.maquinas, construtiva.grafo)
+    tempo_execucao_contrutiva = fim_construtiva - inicio_construtiva
+    print(f"Tempo de execução: {tempo_execucao_contrutiva:.6f} segundos\n")
 
-    for i, maquina in enumerate(solucao, start=1):
-        print(f"Máquina {i}: {list(maquina)}")
-    print("FO: ", calcular_fo(solucao, construtiva.grafo.pesos))
+    for i in range(iteracoes_refinamento):
+        inicio_refinamento = time.time()
 
-    fim = time.time()
+        solucao, tempo_melhor_solucao = simulated_annealing(taxa_resfriamento, equilibrio_termico, temperatura_inicial, temperatura_congelamento, construtiva.maquina.maquinas, tempo_parada, construtiva.grafo)
 
-    tempo_execucao = fim - inicio
-    print(f"Tempo de execução: {tempo_execucao:.6f} segundos")
+        fim_refinamento = time.time()
+
+        print("------------------------------------------")
+        print(f"Melhor Solução Encontrada {i}:")
+
+        exibir_maquina(solucao, construtiva.grafo.pesos)
+
+        tempo_execucao_refinamento = fim_refinamento - inicio_refinamento
+        print(f"Tempo de execução {i}: {tempo_execucao_refinamento:.6f} segundos")
+        print(f"Instante em que a melhor solução foi encontrada: {tempo_melhor_solucao:.6f} segundos\n")
 
 
-execucao(4)
-# execucao(7)
-# execucao(8)
-# execucao(9)
-# execucao(10)
+execucao(6)
+execucao(8)
+execucao(10)
--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,5 +1,6 @@
 import math
 import random
+import time
 
 from Vizinho import Vizinho
 from Maquina import calcular_fo
@@ -15,22 +16,29 @@ def simulated_annealing(taxa_resfriamento,
                         temperatura_inicial,
                         temperatura_congelamento,
                         solucao_inicial,
+                        tempo_parada,
                         grafo):
+    inicio = time.time()
 
     melhor_solucao = solucao_inicial
     solucao_atual = solucao_inicial
+    tempo_melhor_solucao = time.time() - inicio
     iteracoes_temperatura = 0
     temperatura_corrente = temperatura_inicial
+    tempo_execucao = 0
 
-    while temperatura_corrente > temperatura_congelamento:
+    while temperatura_corrente > temperatura_congelamento and tempo_execucao <= tempo_parada:
+
         while iteracoes_temperatura < equilibrio_termico:
             iteracoes_temperatura += 1
             vizinho = gerar_vizinho(solucao_atual, grafo)
             delta = calcular_fo(vizinho, grafo.pesos) - calcular_fo(solucao_atual, grafo.pesos)
+
             if delta < 0:
                 solucao_atual = vizinho
                 if calcular_fo(vizinho, grafo.pesos) < calcular_fo(melhor_solucao, grafo.pesos):
                     melhor_solucao = vizinho
+                    tempo_melhor_solucao = time.time() - inicio
             else:
                 parcela_aleatoria = random.random()
                 if parcela_aleatoria < math.exp(-delta/temperatura_corrente):
@@ -39,4 +47,8 @@ def simulated_annealing(taxa_resfriamento,
         temperatura_corrente = temperatura_corrente * taxa_resfriamento
         iteracoes_temperatura = 0
 
-    return melhor_solucao
+        fim = time.time()
+
+        tempo_execucao = fim - inicio
+
+    return melhor_solucao, tempo_melhor_solucao
